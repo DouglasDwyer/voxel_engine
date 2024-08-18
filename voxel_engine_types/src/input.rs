dd452b15f110950fd6615e2d1551cf63ae77a718
Douglas M. Dwyer <dougmdwyer@hotmail.com>
2024-08-18 11:42:01 -0400

    Refactor digital/analog inputs to be cleaner

--- voxel_engine_types/src/input.rs
+++ voxel_engine_types/src/input.rs
@@ -1,38 +1,127 @@
+use private::*;
 use serde::*;
+use serde::de::*;
+use std::hash::*;
+use std::marker::*;
 use wings::*;
 
 /// Allows for reading from the user's input devices.
 #[system_trait(host)]
 pub trait Input: 'static {
     /// Gets a handle referencing the given analog action,
-    /// which may take on a continuous range of values between `0.0` and `1.0`.
+    /// which may take on a continuous range of values.
     /// The action is created if it does not exist.
-    fn define_analog(&mut self, descriptor: ActionDescriptor) -> AnalogActionId;
+    #[doc(hidden)]
+	fn define_analog(&mut self, descriptor: ActionDescriptor<Analog>) -> ActionId<Analog>;
     
-    /// Gets a handle referencing the given analog action,
+    /// Gets a handle referencing the given digital action,
     /// which may be either `true` or `false`. The action is created if it does not exist.
-    fn define_digital(&mut self, descriptor: ActionDescriptor) -> DigitalActionId;
+    #[doc(hidden)]
+	fn define_digital(&mut self, descriptor: ActionDescriptor<Digital>) -> ActionId<Digital>;
 
     /// Gets the current value of the provided analog action.
-    fn get_analog(&self, id: AnalogActionId) -> f32;
+    #[doc(hidden)]
+	fn get_analog(&self, id: ActionId<Analog>) -> f32;
     
     /// Gets the current value of the provided digital action.
-    fn get_digital(&self, id: DigitalActionId) -> DigitalActionResult;
+    #[doc(hidden)]
+	fn get_digital(&self, id: ActionId<Digital>) -> DigitalResult;
+}
+
+impl dyn Input {
+    /// Gets a handle referencing the given action, registering the action if it did not exist.
+	pub fn define<I: InputKind>(&mut self, descriptor: ActionDescriptor<I>) -> ActionId<I> {
+		I::define(self, descriptor)
+	}
+
+    /// Gets the current value of the provided action.
+	pub fn get<I: InputKind>(&self, id: ActionId<I>) -> I::Result {
+		I::get(self, id)
+	}
+}
+
+/// Inputs that return a continuous range of values.
+/// The neutral/default value returned is 0.0.
+#[derive(Copy, Clone, Debug, Hash, Default, PartialEq, Eq)]
+pub struct Analog;
+
+/// Inputs that return either `true` or `false`.
+/// The neutral/default value returned is `false`.
+#[derive(Copy, Clone, Debug, Hash, Default, PartialEq, Eq)]
+pub struct Digital;
+
+/// Identifies a certain kind of input.
+pub trait InputKind: Sealed + Sized {
+	/// The type that identifies buttons or joysticks of this kind on user input devices.
+	type Binding: Copy + std::fmt::Debug + PartialEq + Serialize + DeserializeOwned;
+
+	/// The type of value returned when querying this input.
+	type Result: Copy + std::fmt::Debug + PartialEq + Serialize + DeserializeOwned;
+
+	/// Defines a new action of this type.
+	fn define(input: &mut dyn Input, descriptor: ActionDescriptor<Self>) -> ActionId<Self>;
+
+	/// Gets the state of the given action.
+	fn get(input: &dyn Input, id: ActionId<Self>) -> Self::Result;
+}
+
+impl InputKind for Analog {
+	type Binding = AnalogInput;
+
+	type Result = f32;
+	
+	fn define(input: &mut dyn Input, descriptor: ActionDescriptor<Self>) -> ActionId<Self> {
+		input.define_analog(descriptor)
+	}
+	
+	fn get(input: &dyn Input, id: ActionId<Self>) -> Self::Result {
+		input.get_analog(id)
+	}
+}
+
+impl InputKind for Digital {
+	type Binding = DigitalInput;
+
+	type Result = DigitalResult;
+	
+	fn define(input: &mut dyn Input, descriptor: ActionDescriptor<Self>) -> ActionId<Self> {
+		input.define_digital(descriptor)
+	}
+	
+	fn get(input: &dyn Input, id: ActionId<Self>) -> Self::Result {
+		input.get_digital(id)
+	}
+}
+
+/// Identifies an action that has been bound for user input.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct ActionId<I: InputKind>(u64, PhantomData<fn(I)>);
+
+impl<I: InputKind> From<u64> for ActionId<I> {
+    fn from(value: u64) -> Self {
+        Self(value, PhantomData)
+    }
+}
+
+impl<I: InputKind> From<ActionId<I>> for u64 {
+    fn from(value: ActionId<I>) -> Self {
+        value.0
+    }
 }
 
 /// Identifies an action and describes its default parameters.
 #[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
-pub struct ActionDescriptor {
+pub struct ActionDescriptor<I: InputKind> {
     /// A list of default buttons. The first button that exists when
     /// this action is created will automatically be bound to it.
-    pub default_bindings: Vec<InputBinding>,
+    pub default_bindings: Vec<I::Binding>,
 	/// The name of the action.
 	pub name: ActionName
 }
 
-impl ActionDescriptor {
+impl<I: InputKind> ActionDescriptor<I> {
     /// Creates a new descriptor associated with the given system.
-    pub fn new<S: wings::marshal::ExportType>(name: ActionName, default_bindings: &[InputBinding]) -> Self {
+    pub fn new(name: ActionName, default_bindings: &[I::Binding]) -> Self {
         Self {
             default_bindings: default_bindings.to_vec(),
             name
@@ -61,7 +150,7 @@ impl ActionName {
 
 /// Describes the current state of a digital action.
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
-pub struct DigitalActionResult {
+pub struct DigitalResult {
     /// Whether the button is currently pressed.
     pub held: bool,
     /// Whether the button was held last frame but not this frame.
@@ -70,50 +159,36 @@ pub struct DigitalActionResult {
     pub pressed: bool
 }
 
-/// Identifies an analog action, which can take on a continuous range of values between `0.0` and `1.0`.
+/// Identifies a source to which an analog action may be bound.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
-pub struct AnalogActionId(u64);
-
-impl From<u64> for AnalogActionId {
-    fn from(value: u64) -> Self {
-        Self(value)
-    }
+pub enum AnalogInput {
+	/// An axis on a mouse.
+	Mouse(MouseAxis)
 }
 
-impl From<AnalogActionId> for u64 {
-    fn from(value: AnalogActionId) -> Self {
-        value.0
-    }
-}
-
-/// Identifies a digital action, which can either be `true` or `false`.
+/// Identifies a continuous axis on a mouse.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
-pub struct DigitalActionId(u64);
-
-impl From<u64> for DigitalActionId {
-    fn from(value: u64) -> Self {
-        Self(value)
-    }
+pub enum MouseAxis {
+	/// The horizontal wheel axis. Returns a real number corresponding to the number
+	/// of wheel "clicks" that occurred on the current frame.
+	HorizontalWheel,
+	/// The vertical wheel axis. Returns a real number corresponding to the number
+	/// of wheel "clicks" that occurred on the current frame.
+	VerticalWheel
 }
 
-impl From<DigitalActionId> for u64 {
-    fn from(value: DigitalActionId) -> Self {
-        value.0
-    }
-}
-
-/// Identifies a source to which an action may be bound.
+/// Identifies a source to which a digital action may be bound.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
-pub enum InputBinding {
+pub enum DigitalInput {
     /// A key on a keyboard.
-    Keyboard(KeyInput),
-    /// A button or mouse wheel.
-    Mouse(MouseInput),
+    Keyboard(Key),
+    /// A mouse button.
+    Mouse(MouseButton),
 }
 
 /// Denotes a key on a user's keyboard.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
-pub enum KeyInput {
+pub enum Key {
 	// Alphabet
 	A,
 	B,
@@ -243,13 +318,22 @@ pub enum KeyInput {
 
 /// Indicates a button on the user's mouse.
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
-pub enum MouseInput {
+pub enum MouseButton {
     /// The left mouse button.
     Left,
     /// The center mouse button.
     Middle,
     /// The right mouse button.
-    Right,
-    /// The analog scrolling wheel.
-    Wheel
+    Right
+}
+
+/// Hides internal implementation details.
+mod private {
+	use super::*;
+
+	/// Prevents third-party crates from implementing a trait.
+	pub trait Sealed {}
+
+	impl Sealed for Analog {}
+	impl Sealed for Digital {}
 }
